f41b1ef7d4dad0f0e7e41d8cec09f6c38fe0691b
Julian Lindner <julian.lindner@vonaffenfels.de>
2022-04-28 14:26:52 +0200

    v 1.0.15

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,5 @@
+import "regenerator-runtime/runtime";
+
 import ApolloContext, {ApolloContextProvider} from "./context/ApolloContext";
 import CartContext, {CartContextProvider} from "./context/CartContext";
 import CheckoutContext, {CheckoutContextProvider} from "./context/CheckoutContext";
