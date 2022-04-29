dcdefe83b9a38be68ab402361225cc132c4ed3f4
Julian Lindner <julian.lindner@vonaffenfels.de>
2022-04-29 08:04:59 +0200

    v 1.0.26

--- src/cart.jsx
+++ src/cart.jsx
@@ -2,9 +2,15 @@ import React, {Fragment, useContext, useState} from "react";
 import CartContext from "./context/CartContext";
 
 const Cart = ({...props}) => {
-    const {addItemToCart} = useContext(CartContext);
+    const cartContext = useContext(CartContext);
+    const {addItemToCart} = cartContext;
+    console.log("cartContext", cartContext);
 
     const [variantId, setVariantId] = useState("");
+
+    const onChange = (e) => {
+        setVariantId(e.target.value);
+    };
 
     const onClick = async () => {
         await addItemToCart(variantId);
@@ -12,7 +18,7 @@ const Cart = ({...props}) => {
 
     return (
         <Fragment>
-            <input onChange={setVariantId} value={variantId} type="text" />
+            <input onChange={onChange} value={variantId} type="text" />
             <input onClick={onClick} type="button" />
         </Fragment>
     );
