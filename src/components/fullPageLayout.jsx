bd992d9285441bcdb9bc75effa5ba13a03600a1a
Julian Lindner <julian.lindner@vonaffenfels.de>
2022-05-11 14:25:22 +0200

    v. 1.0.74

--- src/components/fullPageLayout.jsx
+++ src/components/fullPageLayout.jsx
@@ -5,7 +5,7 @@ const FullPageLayout = ({show, onClose, children}) => (
     <Transition.Root show={show} as={Fragment}>
         <Dialog as="div" className="fixed inset-0 overflow-hidden" onClose={onClose}>
             <div className="absolute inset-0 overflow-hidden bg-white bg-slate-50">
-                <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full">
                     {children}
                 </div>
             </div>
