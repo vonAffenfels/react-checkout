78bfc4b4a2a7206f107a35a82fca234052f39928
Julian Lindner <julian.lindner@vonaffenfels.de>
2022-05-11 07:58:29 +0200

    v. 1.0.68

--- src/cartFullPage.jsx
+++ src/cartFullPage.jsx
@@ -1,4 +1,4 @@
-import React, {Fragment, useContext} from "react";
+import React, {Fragment, useContext, useEffect, useState} from "react";
 import {Dialog, Transition} from "@headlessui/react";
 
 import CheckoutContext from "./context/CheckoutContext";
@@ -9,27 +9,36 @@ const CartFullPage = ({props}) => {
         checkout,
         setDisplayState
     } = useContext(CheckoutContext);
+    const [isMounting, setMounting] = useState(true);
+
+    useEffect(() => {
+        setMounting(false);
+    }, []);
+
+    console.log("isMounting", isMounting);
 
     return (
-        <div className="fixed inset-0 overflow-hidden">
-            <div className="absolute inset-0 overflow-hidden">
-                <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
-                    <Transition.Child
-                        as={Fragment}
-                        enter="transform transition ease-in-out duration-500 sm:duration-700"
-                        enterFrom="translate-x-full"
-                        enterTo="translate-x-0"
-                        leave="transform transition ease-in-out duration-500 sm:duration-700"
-                        leaveFrom="translate-x-0"
-                        leaveTo="translate-x-full"
-                    >
-                        <Dialog.Panel className="pointer-events-auto w-screen">
-                            <CloseButton onClick={() => setDisplayState("widget")} />
-                        </Dialog.Panel>
-                    </Transition.Child>
+        <Transition.Root show={true} as={Fragment}>
+            <div className="fixed inset-0 overflow-hidden">
+                <div className="absolute inset-0 overflow-hidden">
+                    <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="transform transition ease-in-out duration-500 sm:duration-700"
+                            enterFrom="translate-x-full"
+                            enterTo="translate-x-0"
+                            leave="transform transition ease-in-out duration-500 sm:duration-700"
+                            leaveFrom="translate-x-0"
+                            leaveTo="translate-x-full"
+                        >
+                            <Dialog.Panel className="pointer-events-auto w-screen">
+                                <CloseButton onClick={() => setDisplayState("widget")} />
+                            </Dialog.Panel>
+                        </Transition.Child>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Transition.Root>
     );
 }
 
