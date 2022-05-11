0be0fa617eed0d13260b758f4d3933190721ebe7
Julian Lindner <julian.lindner@vonaffenfels.de>
2022-05-11 09:03:52 +0200

    v. 1.0.71

--- src/components/closeButton.jsx
+++ src/components/closeButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CloseButton = ({onClick}) => (
     <div className="bg-opacity-50 flex justify-center items-center absolute top-0 right-0">
-        <div className="bg-white px-16 py-14 rounded-md text-center">
+        <div className="px-16 py-14 rounded-md text-center">
             <button
                 className="bg-indigo-500 px-4 py-2 rounded-md text-md text-white"
                 onClick={onClick || null}
