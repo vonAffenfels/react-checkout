5364ab16f8bbec3fd324fe87082300f39e49ab16
Julian Lindner <julian.lindner@vonaffenfels.de>
2022-07-11 12:21:30 +0200

    v. 1.0.193

--- src/components/sidePanelLayout.jsx
+++ src/components/sidePanelLayout.jsx
@@ -1,52 +1,55 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useState} from "react";
 import {Dialog, Transition} from "@headlessui/react";
 
-const SidePanelLayout = ({show, onClose, children}) => (
-    <Transition.Root
-        show={show}
-        as={"div"}
-        beforeLeave={() => console.log("beforeLeave1")}
-        afterLeave={() => console.log("afterLeave1")}
-        enter="ease-in-out duration-500"
-        enterFrom="opacity-0"
-        enterTo="opacity-100"
-        leave="ease-in-out duration-500"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
-        unmount={false}
-    >
-        <Dialog className="relative z-10" onClose={onClose}>
-            <Transition.Child
-                as={Fragment}
-                enter="ease-in-out duration-500"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in-out duration-500"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-            >
-                <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-            </Transition.Child>
+const SidePanelLayout = ({show, onClose, children}) => {
+    const [startLeaving, setStartLeaving] = useState(false);
 
-            <Transition.Child
-                as={Fragment}
-                enter="transform transition ease-in-out duration-500 sm:duration-700"
-                enterFrom="translate-x-full"
-                enterTo="translate-x-0"
-                leave="transform transition ease-in-out duration-500 sm:duration-700"
-                leaveFrom="translate-x-0"
-                leaveTo="translate-x-full"
-            >
-                <div className="fixed inset-0 overflow-hidden">
-                    <div className="absolute inset-0 overflow-hidden">
-                        <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
-                            {children}
+    return (
+        <Transition.Root
+            show={show}
+            as={"div"}
+            beforeLeave={() => console.log("beforeLeave1")}
+            afterLeave={() => console.log("afterLeave1")}
+            enter="ease-in-out duration-500"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in-out duration-500"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+        >
+            <Dialog className="relative z-10" onClose={onClose}>
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-in-out duration-500"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in-out duration-500"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                </Transition.Child>
+
+                <Transition.Child
+                    as={Fragment}
+                    enter="transform transition ease-in-out duration-500 sm:duration-700"
+                    enterFrom="translate-x-full"
+                    enterTo="translate-x-0"
+                    leave="transform transition ease-in-out duration-500 sm:duration-700"
+                    leaveFrom="translate-x-0"
+                    leaveTo="translate-x-full"
+                >
+                    <div className="fixed inset-0 overflow-hidden">
+                        <div className="absolute inset-0 overflow-hidden">
+                            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                                {children}
+                            </div>
                         </div>
                     </div>
-                </div>
-            </Transition.Child>
-        </Dialog>
-    </Transition.Root>
-);
+                </Transition.Child>
+            </Dialog>
+        </Transition.Root>
+    );
+}
 
 export default SidePanelLayout;
