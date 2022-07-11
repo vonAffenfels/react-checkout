8edb4da134a4aec96eaa155bceaf49902d94580f
Julian Lindner <julian.lindner@vonaffenfels.de>
2022-07-11 14:25:52 +0200

    v. 1.0.200

--- src/components/sidePanelLayout.jsx
+++ src/components/sidePanelLayout.jsx
@@ -30,10 +30,10 @@ const SidePanelLayout = ({show, onClose, children}) => {
 
                 <Transition.Child
                     as={Fragment}
-                    enter="transform transition ease-in-out duration-250 sm:duration-700"
+                    enter="transform transition ease-in-out duration-500 sm:duration-500"
                     enterFrom="translate-x-full"
                     enterTo="translate-x-0"
-                    leave="transform transition ease-in-out duration-250 sm:duration-700"
+                    leave="transform transition ease-in-out duration-500 sm:duration-500"
                     leaveFrom="translate-x-0"
                     leaveTo="translate-x-full"
                 >
