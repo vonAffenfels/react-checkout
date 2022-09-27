086a44e64a308ccfe08b4bf70b52eae56b9da584
Julian Lindner <julianlindner1@gmail.com>
2022-09-27 15:18:35 +0200

    v. 1.0.231 add no_css option

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,8 @@ import postcss from "rollup-plugin-postcss";
 import babel from "rollup-plugin-babel";
 import pkg from "./package.json";
 
+//TODO different build without css
+
 export default [
     {
         input: "src/index.jsx",
@@ -29,10 +31,7 @@ export default [
                 minimize: true,
                 inject: {
                     insertAt: "top"
-                },
-                // stringifier: function (_, __, ___) {
-                //     console.log("stringifier", _, __, ___);
-                // }
+                }
             }),
             resolve(),
             commonjs({
@@ -52,7 +51,51 @@ export default [
                 ],
                 runtimeHelpers: true
             }),
-            del({targets: ["dist/*"]})
+            // del({targets: ["dist/*"]})
+        ],
+        external: Object.keys(pkg.peerDependencies || {})
+    },
+    {
+        input: "src/index_no_css.jsx",
+        output: [
+            {
+                file: pkg.main.replace("/index", "/index_no_css"),
+                format: "cjs",
+                exports: "default"
+            },
+            {
+                file: pkg.module.replace("/index", "/index_no_css"),
+                format: "esm",
+                exports: "default"
+            }
+        ],
+        plugins: [
+            postcss({
+                config: {
+                    path: "./postcss.config.js"
+                },
+                extensions: [".css", ".scss"],
+                minimize: true,
+            }),
+            resolve(),
+            commonjs({
+                include: /node-modules/
+            }),
+            babel({
+                exclude: "node_modules/**",
+                extensions: [".js", ".jsx"],
+                presets: [
+                    ["@babel/preset-env", {targets: {esmodules: true}}],
+                    ["@babel/preset-react", {runtime: "automatic"}]
+                ],
+                plugins: [
+                    ["@babel/plugin-transform-runtime", {regenerator: true, helpers: false, /*corejs: 3*/}],
+                    "@babel/plugin-transform-async-to-generator",
+                    "@babel/plugin-transform-react-jsx"
+                ],
+                runtimeHelpers: true
+            }),
+            // del({targets: ["dist/*"]})
         ],
         external: Object.keys(pkg.peerDependencies || {})
     }
